d7e5917512db40967867aa1428ebeed5dd995a36
ouzhigang <ouzhigangGBA@gmail.com>
2014-05-02 07:34:49 +0800

    Update AppDelegate.cpp

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -30,7 +30,7 @@ bool AppDelegate::applicationDidFinishLaunching()
     CCDirector *pDirector = CCDirector::sharedDirector();
     pDirector->setOpenGLView(CCEGLView::sharedOpenGLView());
 
-    CCEGLView::sharedOpenGLView()->setDesignResolutionSize(1024, 960, kResolutionShowAll);
+    CCEGLView::sharedOpenGLView()->setDesignResolutionSize(1024, 768, kResolutionShowAll);
     
     // turn on display FPS
     pDirector->setDisplayStats(false);
